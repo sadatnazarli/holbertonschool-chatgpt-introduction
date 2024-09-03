d4c2371dd092d11e4932e294fca5413b3042cedf
sadatnazarli <sedat.nazarli@gmail.com>
2024-09-03 05:10:10 +0000

    task 0 fix

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,5 +8,10 @@ def factorial(n):
         n -= 1  # Decrement n in each iteration
     return result
 
+if len(sys.argv) < 2:
+    print("Usage: python3 factorial.py <number>")
+    sys.exit(1)
+
 f = factorial(int(sys.argv[1]))
 print(f)
+
